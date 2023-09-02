13c0a87a81b9948042afab07bde4ba8e2c860a86
hosseinahmadizadeh <hosseinahmadizadeh1377@gmail.com>
2023-09-02 17:58:21 +0330

    refactor: update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header/Header";
+import Header from "./components/header/Header";
 import Hero from "./components/Hero/Hero";
 import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
@@ -6,6 +6,7 @@ import Value from "./components/Value/Value";
 import Contact from "./components/Contact/Contact";
 import GetStarted from "./components/GetStarted/GetStarted";
 import Footer from "./components/Footer/Footer";
+
 function App() {
   return (
     <div className="App">
